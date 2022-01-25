078881fcc558536bb703b58e76a782fe6e6a9fca
josh-hern <joh0005@mix.wvu.edu>
2022-01-24 19:11:26 -0500

    Now creates databases
    Signed-off-by: josh-hern <joh0005@mix.wvu.edu>

--- back_end_stuff/construct_database.py
+++ back_end_stuff/construct_database.py
@@ -1,19 +1,44 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DATETIME, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 Base = declarative_base()
 
 
-class ShipPlacement(Base):
-    __tablename__ = "ShipPlacement"
+class ShipDesignation(Base):
+    """
+    Creates the ShipDesignation table
+    """
+    __tablename__ = "ShipDesignation"
 
     ship_id = Column('ship_id', Integer, primary_key=True)
     length = Column('length', Integer)
     ship_name = Column('ship_name', String)
 
 
+class ShipPlacement(Base):
+    """
+    Creates the ShipPlacement table
+    """
+    __tablename__ = "ShipPlacement"
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+    id = Column('id', Integer, primary_key=True)
+    player = Column('player', Integer)
+    ship_id = Column('ship_id', Integer, ForeignKey(ShipDesignation.ship_id))
+
+
+class AttackTable(Base):
+    """
+    Creates the attacktable
+    """
+    __tablename__ = "AttackTable"
+
+    time_stamp = Column('time_stamp', DATETIME, primary_key=True)
+    player = Column('player', Integer)
+    space_attacked = Column('space_attacked', String)
+    hit = Column('hit', Boolean)
+
+
+engine = create_engine('sqlite:///../database/battleship_database.db', echo=True)
 Base.metadata.create_all(bind=engine)
 
 
