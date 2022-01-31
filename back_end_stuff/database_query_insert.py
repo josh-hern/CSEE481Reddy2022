e1974816ebb659dce9d06ad9ae87b71846bbde27
josh-hern <52827400+josh-hern@users.noreply.github.com>
2022-01-30 20:31:00 -0500

    remade methods in database_query_insert.py into class
    Signed-off-by: josh-hern <52827400+josh-hern@users.noreply.github.com>

--- back_end_stuff/database_query_insert.py
+++ back_end_stuff/database_query_insert.py
@@ -6,68 +6,63 @@ from sqlalchemy import create_engine, insert
 from sqlalchemy.orm import sessionmaker, relationship
 
 
-def initialization():
-    # trying to add rows to database table. Will work on more later
-    engine = create_engine('sqlite:///../database/battleship_database.db', echo=True)
-    session = sessionmaker(bind=engine)
+class Initialization:
+    def __init__(self):
+        engine = create_engine('sqlite:///../database/battleship_database.db', echo=True)
+        self.sessionmaker_session = sessionmaker(bind=engine)
 
-    create_carrier(session)
-    create_battleship(session)
-    create_cruiser(session)
-    create_destroyer(session)
-    create_submarine(session)
+        self.create_carrier()
+        self.create_battleship()
+        self.create_cruiser()
+        self.create_destroyer()
+        self.create_submarine()
 
+    def create_carrier(self):
+        session = self.sessionmaker_session()
+        carrier = ShipDesignation()
+        carrier.id = 1
+        carrier.length = 5
+        carrier.ship_name = "Carrier"
+        session.add(carrier)
+        session.commit()
+        session.close()
 
-def create_carrier(sessionmaker_session):
-    session = sessionmaker_session()
-    carrier = ShipDesignation()
-    carrier.id = 1
-    carrier.length = 5
-    carrier.ship_name = "Carrier"
-    session.add(carrier)
-    session.commit()
-    session.close()
+    def create_battleship(self):
+        session = self.sessionmaker_session()
+        carrier = ShipDesignation()
+        carrier.id = 2
+        carrier.length = 4
+        carrier.ship_name = "Battleship"
+        session.add(carrier)
+        session.commit()
+        session.close()
 
+    def create_cruiser(self):
+        session = self.sessionmaker_session()
+        carrier = ShipDesignation()
+        carrier.id = 3
+        carrier.length = 3
+        carrier.ship_name = "Cruiser"
+        session.add(carrier)
+        session.commit()
+        session.close()
 
-def create_battleship(sessionmaker_session):
-    session = sessionmaker_session()
-    carrier = ShipDesignation()
-    carrier.id = 2
-    carrier.length = 4
-    carrier.ship_name = "Battleship"
-    session.add(carrier)
-    session.commit()
-    session.close()
+    def create_submarine(self):
+        session = self.sessionmaker_session()
+        carrier = ShipDesignation()
+        carrier.id = 4
+        carrier.length = 3
+        carrier.ship_name = "Submarine"
+        session.add(carrier)
+        session.commit()
+        session.close()
 
-
-def create_cruiser(sessionmaker_session):
-    session = sessionmaker_session()
-    carrier = ShipDesignation()
-    carrier.id = 3
-    carrier.length = 3
-    carrier.ship_name = "Cruiser"
-    session.add(carrier)
-    session.commit()
-    session.close()
-
-
-def create_submarine(sessionmaker_session):
-    session = sessionmaker_session()
-    carrier = ShipDesignation()
-    carrier.id = 4
-    carrier.length = 3
-    carrier.ship_name = "Submarine"
-    session.add(carrier)
-    session.commit()
-    session.close()
-
-
-def create_destroyer(sessionmaker_session):
-    session = sessionmaker_session()
-    carrier = ShipDesignation()
-    carrier.id = 5
-    carrier.length = 2
-    carrier.ship_name = "Destroyer"
-    session.add(carrier)
-    session.commit()
-    session.close()
+    def create_destroyer(self):
+        session = self.sessionmaker_session()
+        carrier = ShipDesignation()
+        carrier.id = 5
+        carrier.length = 2
+        carrier.ship_name = "Destroyer"
+        session.add(carrier)
+        session.commit()
+        session.close()
