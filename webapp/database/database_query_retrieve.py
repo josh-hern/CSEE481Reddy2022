fdc2841a1423b555312a683e3b64c990c891125b
josh-hern <52827400+josh-hern@users.noreply.github.com>
2022-02-28 15:57:00 -0500

    made it work with real SQL query, will be trying with ORM
    Signed-off-by: josh-hern <52827400+josh-hern@users.noreply.github.com>

--- webapp/database/database_query_retrieve.py
+++ webapp/database/database_query_retrieve.py
@@ -3,14 +3,20 @@ This file will be for handling database retrieval
 """
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-import construct_database as database
 
 
 def get_occupied_spaces(database_location, space):
     engine = create_engine(f'sqlite:{database_location}', echo=True)
-    sessionmaker_session = sessionmaker(bind=engine)()
-    occupied_spaces = database.OccupiedSpacesTable()
-    sessionmaker_session.query(occupied_spaces.Position).filter(occupied_spaces.Position.like(space))
-    pass
+    connection = engine.connect()
+    table_name = "OccupiedSpaces"
+    query = f'SELECT Position FROM {table_name} WHERE (Position LIKE \"{space}\");'
+    result = connection.execute(query)
+    print(list(result))
+    # sessionmaker_session = sessionmaker(bind=engine)()
+    # occupied_spaces = database.OccupiedSpacesTable()
+    # # print(occupied_spaces.__table__.columns)
+    # q = sessionmaker_session.query(occupied_spaces.__table__).filter(occupied_spaces.Position == space)
+    # return q
+
+
+print(get_occupied_spaces('///battleship_database.db', 'a4'))
