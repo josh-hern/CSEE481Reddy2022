fb7f8ca15dd6197fd91a69f70cc150de45fc28da
Wren Parker <wrenparker@gmail.com>
2022-03-06 18:47:03 -0500

    setup other pages

--- webapp/api/routes.py
+++ webapp/api/routes.py
@@ -5,8 +5,20 @@ api = Blueprint('/api/', __name__)
 
 @api.route('/api/')
 def test():
-    return 'hey'
+    return 'hey uWu'
 
 @api.route('/')
 def homeRoute():
     return viewHome()
+
+@api.route('/about')
+def aboutRoute():
+    return viewAbout()
+
+@api.route('/history')
+def historyRoute():
+    return viewHistory()
+
+@api.route('/merch')
+def merchRoute():
+    return viewMerch()
--- webapp/controllers/BaseController.py
+++ webapp/controllers/BaseController.py
@@ -2,4 +2,13 @@ from flask import Blueprint, render_template
 import os
 
 def viewHome():
-    return render_template('pages/home/home.html', test=os.getcwd())
+    return render_template('pages/home/home.html')
+
+def viewAbout():
+    return render_template('pages/about/about.html')
+
+def viewHistory():
+    return render_template('pages/history/history.html')
+
+def viewMerch():
+    return render_template('pages/merch/merch.html')
