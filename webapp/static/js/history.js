a6719cf4ebb3064a93ae47934879ba51a68cc101
josh-hern <52827400+josh-hern@users.noreply.github.com>
2022-04-21 21:04:23 -0400

    fixed stats, and made table spacing better by using nbsp characters
    Signed-off-by: josh-hern <52827400+josh-hern@users.noreply.github.com>

--- webapp/static/js/history.js
+++ webapp/static/js/history.js
@@ -41,8 +41,12 @@ const createTable = (gameStats) => {
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    let cell5 = row.insertCell(4);
     cell1.innerHTML = dict_key;
-    cell2.innerHTML = gameStats[dict_key][0];
-    cell3.innerHTML = gameStats[dict_key][1];
+    cell2.innerHTML = "     "
+    cell3.innerHTML = gameStats[dict_key][0];
+    cell4.innerHTML = "     "
+    cell5.innerHTML = gameStats[dict_key][1];
   }
 }
